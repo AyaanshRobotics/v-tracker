29f08f9ed7fc94513dfaeeb7f37937c5df971895
AyaanshRobotics <superuser5698@outlook.com>
2026-03-02 19:08:48 +1100

    add main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+def run(name):
+    if name == "init":
+        import subprocess
+        import os
+        subprocess.run(['mkdir', 'v-track'],shell=True)
+        subprocess.run(['cd', 'v-track'],shell=True)
+        file = input("file: ") 
+        print("addding", file, "to tracker.txt")
+        with open("v-track/tracker.txt", mode="a+") as f:
+      #      stoof = f.read()
+       #     print(stoof)
+        #    if file in stoof:
+         #       print("sorry duplicates will crash the thing")
+          #      exit(0)
+            f.write(file)
+            f.write("\n")
+    elif name == "read":
+        with open("v-track/tracker.txt", mode="r") as f:
+            print(f"files that are being tracked:\n{f.read()}")
+    elif name == "add":
+        option = 
+
+arg = argparse.ArgumentParser(description="git like program")
+arg.add_argument("action", help="do something")
+args = arg.parse_args()
+run(args.action)

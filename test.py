58d2ec509e94d017dc8fb6459a806586bbafd47f
Cyan <superuser5698@outlook.com>
2026-03-15 09:08:46 +1100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import pythoncom
-
-pythoncom.CoInitialize()
-print("Hello, World!")
-
-ssssd = "1111111111111111111111111111111111111111111111111111dddddddddddddddddddddddddddddddddddd'''''''''''''''''''''''''''''''"
